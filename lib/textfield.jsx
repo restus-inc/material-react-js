e6a8d1c57a71e3ed8b00ebfd7a560ae7fff0364c
YuoMamoru <myuo@cam.hi-ho.ne.jp>
2020-06-11 12:16:28 +0900

    :+1: Support value property on textfield

--- lib/textfield.jsx
+++ lib/textfield.jsx
@@ -32,11 +32,15 @@ const generateRootClassName = (props) => {
 function NativeInput(props) {
   const nativeInputProps = {
     className: 'mdc-text-field__input',
-    defaultValue: props.defaultValue || '',
     disabled: Boolean(props.disabled),
     required: Boolean(props.required),
     onChange: props.onChange,
   };
+  if (props.value == null) {
+    Object.assign(nativeInputProps, { defaultValue: props.defaultValue });
+  } else {
+    Object.assign(nativeInputProps, { value: props.value });
+  }
   if (props.dataset) {
     Object.assign(
       nativeInputProps,
@@ -94,7 +98,10 @@ function HelperText(props) {
  * @param {Object} [props]
  * @param {string} [props.id] The `id` attribute of the native html input element. This
  * property is also used the `name` attribute.
- * @param {string} [props.defaultValue] The default value of the text field.
+ * @param {string} [props.value] The value of the text field. Should not specify both
+ * this attribute and `props.defaultValue` at the same time.
+ * @param {string} [props.defaultValue] The default value of the text field. Should not
+ * specify both this attribute and `props.value` at the same time.
  * @param {string} [props.type] The `type` attribute of the native html input element.
  * If `props.variation` is `'textarea'`, this attribute is ignored. Default to `'text'`.
  * @param {string} [props.variation] The variation of the text field. Supported variations
@@ -124,11 +131,21 @@ function HelperText(props) {
  */
 export default function TextField(props) {
   const rootElement = useRef();
+  const mdcComponentRef = useRef();
 
   useEffect(() => {
-    const mdcComponent = new MDCTextField(rootElement.current);
+    const hasFocus = document.hasFocus() && document.activeElement === rootElement.current;
+    if (mdcComponentRef.current && !hasFocus) {
+      mdcComponentRef.current.useNativeValidation = false;
+      mdcComponentRef.current.value = props.value;
+      mdcComponentRef.current.useNativeValidation = true;
+    }
+  }, [props.value]);
+
+  useEffect(() => {
+    mdcComponentRef.current = new MDCTextField(rootElement.current);
     return () => {
-      mdcComponent.destroy();
+      mdcComponentRef.current.destroy();
     };
   }, []);
 
